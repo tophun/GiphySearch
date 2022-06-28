5b686c06e4175f3b5ae13c2c8a7bc0b624879107
Collie <imtophun@gmail.com>
2022-06-29 08:11:30 +0900

    feat: pod install

---
+++ Pods/Target Support Files/Pods-GiphySearch-GiphySearchUITests/Pods-GiphySearch-GiphySearchUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_GiphySearch_GiphySearchUITests : NSObject
+@end
+@implementation PodsDummy_Pods_GiphySearch_GiphySearchUITests
+@end
---
+++ Pods/Target Support Files/Pods-GiphySearchTests/Pods-GiphySearchTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_GiphySearchTests : NSObject
+@end
+@implementation PodsDummy_Pods_GiphySearchTests
+@end
